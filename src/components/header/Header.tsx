074fbaa503b419433cf86f5d97abbf0f2b889ad7
An0510 <827479225@qq.com>
2022-01-25 09:31:57 +0800

    提交

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -110,22 +110,22 @@ export const Header: React.FC = () => {
                 />
             </Layout.Header>
             <Menu mode={"horizontal"} className={styles["main-menu"]}>
-                <Menu.Item key={1} onClick={() => history.push(publicPath)}>旅游首页</Menu.Item>
-                <Menu.Item key={2}>周末游</Menu.Item>
-                <Menu.Item key={3}>跟团游</Menu.Item>
-                <Menu.Item key="4"> 自由行 </Menu.Item>
-                <Menu.Item key="5"> 私家团 </Menu.Item>
-                <Menu.Item key="6"> 邮轮 </Menu.Item>
-                <Menu.Item key="7"> 酒店+景点 </Menu.Item>
-                <Menu.Item key="8"> 当地玩乐 </Menu.Item>
-                <Menu.Item key="9"> 主题游 </Menu.Item>
-                <Menu.Item key="10"> 定制游 </Menu.Item>
-                <Menu.Item key="11"> 游学 </Menu.Item>
-                <Menu.Item key="12"> 签证 </Menu.Item>
-                <Menu.Item key="13"> 企业游 </Menu.Item>
-                <Menu.Item key="14"> 高端游 </Menu.Item>
-                <Menu.Item key="15"> 爱玩户外 </Menu.Item>
-                <Menu.Item key="16"> 保险 </Menu.Item>
+                <Menu.Item key={1} onClick={() => history.push(publicPath)}>{t("header.home_page")}</Menu.Item>
+                <Menu.Item key="2"> {t("header.weekend")} </Menu.Item>
+                <Menu.Item key="3"> {t("header.group")} </Menu.Item>
+                <Menu.Item key="4"> {t("header.backpack")} </Menu.Item>
+                <Menu.Item key="5"> {t("header.private")} </Menu.Item>
+                <Menu.Item key="6"> {t("header.cruise")} </Menu.Item>
+                <Menu.Item key="7"> {t("header.hotel")} </Menu.Item>
+                <Menu.Item key="8"> {t("header.local")} </Menu.Item>
+                <Menu.Item key="9"> {t("header.theme")} </Menu.Item>
+                <Menu.Item key="10"> {t("header.custom")} </Menu.Item>
+                <Menu.Item key="11"> {t("header.study")} </Menu.Item>
+                <Menu.Item key="12"> {t("header.visa")} </Menu.Item>
+                <Menu.Item key="13"> {t("header.enterprise")} </Menu.Item>
+                <Menu.Item key="14"> {t("header.high_end")} </Menu.Item>
+                <Menu.Item key="15"> {t("header.outdoor")} </Menu.Item>
+                <Menu.Item key="16"> {t("header.insurance")} </Menu.Item>
             </Menu>
         </div>
     );
